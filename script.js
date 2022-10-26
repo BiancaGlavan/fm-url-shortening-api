78682e2c78985150de9ed3daff6565513b1eb89e
BiancaGlavan <bianca.glavan@yahoo.ro>
2022-10-26 22:22:30 +0300

    first commit desktop design

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    // var text = "Example text to appear on clipboard";
+
+    // if (navigator.clipboard) {
+    //     // yes 😎 
+    //     navigator.clipboard.writeText(text).then(function () {
+    //         console.log('Async: Copying to clipboard was successful!');
+    //     }, function (err) {
+    //         console.error('Async: Could not copy text: ', err);
+    //     });
+    // }
+
+
+
+});
