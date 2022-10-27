dd83fd6a890e07216a0c656a0a610efd159ca656
BiancaGlavan <bianca.glavan@yahoo.ro>
2022-10-27 21:17:25 +0300

    add functionality with js

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+const shortenInput = document.querySelector('.shorten-input');
+const shortenButton = document.querySelector('.shorten-button');
+const apiUrl = 'https://api.shrtco.de/v2/shorten?url=';
+const urlList = document.querySelector('.url-list');
+const shortenUrl = document.querySelector('.shorten-url');
+const errorMSg = document.querySelector('.error-msg');
+
+const App = {
+
+
+    shortenLink: () => {
+        console.log('shorten button clicked');
+        const url = apiUrl + shortenInput.value;
+
+
+        fetch(url, {
+            method: 'POST', // or 'PUT'
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+
+                if (!data.error) {
+                    let newShortUrl = data.result.full_short_link2;
+                    const existingButton = document.querySelector(`[data-url="${newShortUrl}"]`);
+                    if (existingButton) {
+                        errorMSg.innerHTML = 'This URL already exist';
+                        //print error: This URL already exist!
+                        console.log('This URL already exist!');
+                    } else {
+                        App.printShortenLink(data);
+                        errorMSg.innerHTML = '';
+                    }
+                } else {
+                    errorMSg.innerHTML = data.error;
+                }
+
+
+
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+    },
+
+    printShortenLink: (data) => {
+        let res = '';
+        let newInput = `<div class="shorten-url">
+        <span class="original-url">${shortenInput.value}</span>
+        <div class="url-right">
+          <span class="transformed-url">${data.result.full_short_link2}</span>
+          <button class="btn url-button" data-url="${data.result.full_short_link2}" onclick="App.copyToClipboard('${data.result.full_short_link2}')">Copy</button>
+        </div>
+      </div>`;
+
+        res = res + newInput;
+        urlList.innerHTML = urlList.innerHTML + res;
+
+
+    },
+
+    copyToClipboard: (url) => {
+
+
+        if (navigator.clipboard) {
+
+            navigator.clipboard.writeText(url).then(function () {
+                console.log('Async: Copying to clipboard was successful!');
+                const selectedButton = document.querySelector(`[data-url="${url}"]`);
+
+                document.querySelectorAll('.url-button').forEach((el, idx) => {
+                    el.classList.remove('selected');
+                    el.innerHTML = `Copy`;
+                })
+
+
+                selectedButton.innerHTML = `Copied`;
+                selectedButton.classList.add('selected');
+            }, function (err) {
+                console.error('Async: Could not copy text: ', err);
+            });
+        }
+    }
+
+}
+
+
+
+
+shortenButton.addEventListener('click', App.shortenLink);
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -16,3 +108,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 });
+
